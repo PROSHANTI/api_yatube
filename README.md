ed690b41d9a46506a57abf154794a8a3cd5df08f
Maxim Subbotin <pro.shanti@yandex.ru>
2023-11-21 16:49:46 +0300

    1 review

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# api_final
-api final
+# Проект API для Yatube
+
+### Описание:
+
+Финальный проект блока API.
+
+Yatube — это платформа для блогов, чем-то похожая на Блогикум. Все блоги чем-то похожи друг на друга: любой блог-сервис предполагает возможность зарегистрироваться, создать, отредактировать или удалить собственный пост, прокомментировать пост другого автора и подписаться на него.
+
+Данный проект позволяет осуществлять работу с приложением Yatube по средством API на базе Django + Django REST Framework.
+
+### Технологии:
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![DjangoREST](https://img.shields.io/badge/DJANGO-REST-ff1709?style=for-the-badge&logo=django&logoColor=white&color=ff1709&labelColor=gray)
+![JWT](https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens)
+
+### Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке:
+```bash
+git clone https://
+cd api_final_yatube
+```
+
+Cоздать и активировать виртуальное окружение:
+```bash
+WINDOWS:
+
+python -m venv venv
+source venv/scripts/activate
+
+MACOS:
+
+python3 -m venv venv
+```
+Установить зависимости из файла requirements.txt:
+```bash
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+```bash
+cd yatube_api
+python3 manage.py migrate
+```
+
+Запустить проект:
+```bash
+python3 manage.py runserver
+```
+
+### Примеры работы API:
+
+После запуска проекта, докумнетация с примерами доступна по адресу: 
+```
+http://127.0.0.1:8000/redoc/
+```
+
+### Автор проекта
+
+Максим Субботин - [GitHub](<https://github.com/PROSHANTI>)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,69 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from .permissions import IsAuthorOrReadOnly
+from posts.models import Group, Post
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """ViewSet для CRUD операций с постами.
+    Доступно только авторизованному пользователю.
+    """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,
+                          permissions.IsAuthenticatedOrReadOnly)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """ViewSet для CRUD операций с комментариями.
+    Доступно только авторизованному пользователю,
+    редактировать можно только свои комментарии.
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,
+                          permissions.IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        new_queryset = post.comments.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user,
+                        post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet для отображения групп, только чтение."""
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    """ViewSet для подписки.
+    Доступно только автору или в режиме чтения
+    """
+
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthorOrReadOnly,
+                          permissions.IsAuthenticated)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
